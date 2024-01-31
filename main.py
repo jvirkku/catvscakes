cd2ba072131381494d37aaf3e12718883ced81a4
Johanna Virkkunen <johanna.virkkunen@edu.turkuamk.fi>
2024-01-31 11:51:47 +0200

    reversed back to original this def background..it fucked something up :D now everything should be just basic code that we are used to

--- main.py
+++ main.py
@@ -15,16 +15,22 @@ displaySurface = pygame.display.set_mode((width, height))
 screen_rect = displaySurface.get_rect()
 pygame.display.set_caption("Cats in Space")
 
+
 background = pygame.image.load("stars.jpg").convert()
 cat = pygame.image.load("spacecat.png").convert_alpha()
 pie = pygame.image.load("pie.png").convert_alpha()
 
-def draw_backgroung():
-    displaySurface.blit(background, (0,0))
+bullet = pygame.Surface((10,10))
+white = (255,255,255)
+bullet.fill(white)
 
+
+displaySurface.blit(background, (0,0))
 
 displaySurface.blit(cat, (600, 800))
 displaySurface.blit(pie, (0,0))
+displaySurface.blit(bullet, (550,700))
+
 
 pygame.display.flip()
 
@@ -32,9 +38,13 @@ catArea = cat.get_rect(bottomleft = (600,800))
 pieArea = pie.get_rect()
 pieArea.left = random.randrange(0,1280,1) #pie is randomly on the top of the screen
 pieArea.top = 0
+bulletArea = bullet.get_rect()
+bulletArea.top = 700
+bulletArea.left = 700
 
 
 
+speed = [1,1]
 
 while True:
 
@@ -57,13 +67,13 @@ while True:
     if pressings[K_LEFT]:
         catArea.move_ip((-2,0))
     if pressings[K_RIGHT]:
-        catArea.move_ip((2,0))
+        catArea.move_ip((2,0))  
     
     catArea.clamp_ip(screen_rect)
 
-    draw_backgroung() #replaces the need to have dispsurf.blit.background
+    displaySurface.blit(background, (0,0)) #replaces the need to have dispsurf.blit.background
     
     displaySurface.blit(cat, catArea)
     displaySurface.blit(pie, pieArea)
-
+    displaySurface.blit(bullet, bulletArea)
     pygame.display.flip()
