57946472e125879004dd3a767def8fb5cca3d574
Johanna Virkkunen <johanna.virkkunen@edu.turkuamk.fi>
2024-01-31 12:39:25 +0200

    now we have bullet that shoots when pressed space. It only shoots one bullet at a time tho :( but its a start

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from pygame.locals import *
 pygame.init()
 import random
 
-# clock = pygame.time.Clock
-# fps = 60
-
-print("hell yeah this is a game")
 
 width = 1280
 height = 854
@@ -38,17 +34,14 @@ catArea = cat.get_rect(bottomleft = (600,800))
 pieArea = pie.get_rect()
 pieArea.left = random.randrange(0,1280,1) #pie is randomly on the top of the screen
 pieArea.top = 0
-bulletArea = bullet.get_rect()
-bulletArea.top = 700
-bulletArea.left = 700
+bulletArea = bullet.get_rect(topleft=(catArea.centerx, catArea.top)) #bullet is positioned on cat's top center
 
 
+speed = [0,-1]
+bullets = []
 
-speed = [1,1]
 
 while True:
-
-   
 
 
     for event in pygame.event.get():
@@ -59,21 +52,31 @@ while True:
             if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-
-
-
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                bulletArea = bullet.get_rect(topleft=(catArea.centerx, catArea.top))
+                bullets.append(bulletArea) 
+    
 
     pressings = pygame.key.get_pressed()
     if pressings[K_LEFT]:
         catArea.move_ip((-2,0))
     if pressings[K_RIGHT]:
         catArea.move_ip((2,0))  
-    
     catArea.clamp_ip(screen_rect)
+
+    for bulletArea in bullets:
+        bulletArea.move_ip((speed))
+
+   
+    bullets = [bulletArea for bulletArea in bullets if bulletArea.bottom > 0]
+
+
 
     displaySurface.blit(background, (0,0)) #replaces the need to have dispsurf.blit.background
     
     displaySurface.blit(cat, catArea)
     displaySurface.blit(pie, pieArea)
     displaySurface.blit(bullet, bulletArea)
+    
     pygame.display.flip()
