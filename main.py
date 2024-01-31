ce6be0c46bbdd3d30d39af0e51e839cf0958e548
Johanna Virkkunen <johanna.virkkunen@edu.turkuamk.fi>
2024-01-31 11:11:38 +0200

    added fps, and something

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from pygame.locals import *
 pygame.init()
 import random
 
+clock = pygame.time.Clock
+fps = 60
+
 print("hell yeah this is a game")
 
 width = 1280
@@ -14,19 +17,28 @@ pygame.display.set_caption("Cats in Space")
 
 background = pygame.image.load("stars.jpg").convert()
 cat = pygame.image.load("spacecat.png").convert_alpha()
+pie = pygame.image.load("pie.png").convert_alpha()
 
 
 displaySurface.blit(cat, (600, 800))
 displaySurface.blit(background, (0,0))
+displaySurface.blit(pie, (0,0))
 
 pygame.display.flip()
 
 catArea = cat.get_rect(bottomleft = (600,800))
+pieArea = pie.get_rect()
+pieArea.left = random.randrange(0,1280,1) #pie is randomly on the top of the screen
+pieArea.top = 0
+
 
 
 
 while True:
 
+    clock.tick(fps)
+
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -50,5 +62,6 @@ while True:
 
     displaySurface.blit(background, (0,0))
     displaySurface.blit(cat, catArea)
+    displaySurface.blit(pie, pieArea)
 
     pygame.display.flip()
